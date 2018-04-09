8ae1fcfb7700cdda8b47df5e3907d9bc0bab1d4b
ReginM <regina.mihaylischeva@yandex.ru>
2018-04-09 20:04:28 +0600

    Задание 10 MatrixNonSimilarRows

--- src/main/java/net/thumbtack/school/matrix/MatrixNonSimilarRows.java
+++ src/main/java/net/thumbtack/school/matrix/MatrixNonSimilarRows.java
@@ -6,25 +6,15 @@ import java.util.stream.Collectors;
 
 public class MatrixNonSimilarRows {
     private Map<Set<Integer>, int[]> matrixNonSimilarRows;
-    private LinkedHashSet<int[]> setmatrix = new LinkedHashSet<>();
 
     public MatrixNonSimilarRows(int[][] matrix) {
         matrixNonSimilarRows = new HashMap<>();
         for (int i = 0; i < matrix.length; i++) {
-            matrixNonSimilarRows.put(Arrays.stream(matrix[i]).boxed().collect(Collectors.toSet()), matrix[i]);
-            setmatrix.add(matrix[i]);
+            matrixNonSimilarRows.putIfAbsent(Arrays.stream(matrix[i]).boxed().collect(Collectors.toSet()), matrix[i]);
         }
     }
 
     public Set<int[]> getNonSimilarRows() {
-        HashSet<int[]> set1 = new HashSet<>();
-        for (int[] elem : setmatrix) {
-            Set<Integer> mySet1 = Arrays.stream(elem).boxed().collect(Collectors.toSet());
-            if (matrixNonSimilarRows.containsKey(mySet1)) {
-                matrixNonSimilarRows.remove(mySet1);
-                set1.add(elem);
-            }
-        }
-        return set1;
+        return new HashSet<>(matrixNonSimilarRows.values());
     }
 }
