9d4b26fab4e839815d4c66830db910b5e6eb24e0
“Михайлищева <regina.mihaylischeva@gmail.comgit  config --global user.name  git add ./\*.javagit add pom.xmlgit add .gitlab-ci.yml git commit -m first>
2018-09-19 22:12:29 +0600

    задание_12(sql)

--- sql/ttschool.sql
+++ sql/ttschool.sql
@@ -7,7 +7,7 @@ id INT(10) NOT NULL AUTO_INCREMENT,
 firstname VARCHAR(50) NOT NULL,
 lastname VARCHAR(50) NOT NULL,
 rating int (1),
- PRIMARY KEY (id)
+PRIMARY KEY (id)
 )
  ENGINE=INNODB DEFAULT CHARSET=utf8;
 
@@ -22,10 +22,11 @@ ENGINE=INNODB DEFAULT CHARSET=utf8;
 id INT(10) NOT NULL AUTO_INCREMENT,
 name VARCHAR(50) NOT NULL,
 room int(1)  NOT NULL,
-schoolid INT(1) NOT NULL,
+schoolid INT(10) NOT NULL,
+#FOREIGN KEY (schoolid) REFERENCES school.id on delete cascade,
   PRIMARY KEY (id)
 )
- ENGINE=INNODB DEFAULT CHARSET=utf8;
+ENGINE=INNODB DEFAULT CHARSET=utf8;
 
 
  CREATE TABLE school(
@@ -35,3 +36,39 @@ year int(1)  NOT NULL,
 PRIMARY KEY (id)
 )
 ENGINE=INNODB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE school_subject(
+PRIMARY KEY (school_id,subject_id),
+school_id INT(10) NOT NULL,
+subject_id INT(10) NOT NULL
+
+)
+ENGINE=INNODB DEFAULT CHARSET=utf8;
+CREATE TABLE groupe_trainee(
+PRIMARY KEY (trainee_id,groupe_id),
+trainee_id INT(10) NOT NULL,
+groupe_id INT(10) NOT NULL
+
+)
+ENGINE=INNODB DEFAULT CHARSET=utf8;
+
+ALTER TABLE school_subject
+ADD CONSTRAINT school_id
+FOREIGN KEY (school_id) REFERENCES school(id) on delete cascade on update cascade ;
+ALTER TABLE school_subject
+ADD CONSTRAINT subject_id
+FOREIGN KEY (subject_id) REFERENCES subject(id) on delete cascade on update cascade;
+
+
+ALTER TABLE ttschool.groupe
+ADD CONSTRAINT groupee
+FOREIGN KEY (schoolid) REFERENCES school(id) on delete cascade on update cascade;
+
+
+ALTER TABLE groupe_trainee
+ADD CONSTRAINT trainee_id
+FOREIGN KEY (trainee_id) REFERENCES trainee(id) on delete cascade on update cascade ;
+ALTER TABLE groupe_trainee
+ADD CONSTRAINT groupe_id
+FOREIGN KEY (groupe_id) REFERENCES groupe(id) on delete cascade on update cascade;
