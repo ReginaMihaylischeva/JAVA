25f57bf398b8f59654692c582955033525cadd48
“Михайлищева <regina.mihaylischeva@gmail.comgit  config --global user.name  git add ./\*.javagit add pom.xmlgit add .gitlab-ci.yml git commit -m first>
2018-10-05 22:19:50 +0600

    Скрипт(не хочет коммититься вместе с проектом)

--- sql/ttschool.sql
+++ sql/ttschool.sql
@@ -24,7 +24,7 @@ name VARCHAR(50) NOT NULL,
 room int(1)  NOT NULL,
 schoolid INT(10) NOT NULL,
 #FOREIGN KEY (schoolid) REFERENCES school.id on delete cascade,
-  PRIMARY KEY (id)
+PRIMARY KEY (id)
 )
 ENGINE=INNODB DEFAULT CHARSET=utf8;
 
@@ -34,13 +34,13 @@ id INT(10) NOT NULL AUTO_INCREMENT,
 name VARCHAR(50) NOT NULL,
 year int(1)  NOT NULL,
 PRIMARY KEY (id)
-) 
+)
 ENGINE=INNODB DEFAULT CHARSET=utf8;
 
 
-CREATE TABLE school_subject(
-PRIMARY KEY (school_id,subject_id),
-school_id INT(10) NOT NULL,
+CREATE TABLE groupe_subject(
+PRIMARY KEY (group_id,subject_id),
+group_id INT(10) NOT NULL,
 subject_id INT(10) NOT NULL
 
 )
@@ -53,12 +53,12 @@ groupe_id INT(10) NOT NULL
 )
 ENGINE=INNODB DEFAULT CHARSET=utf8;
 
-ALTER TABLE school_subject
-ADD CONSTRAINT school_id
-FOREIGN KEY (school_id) REFERENCES school(id) on delete cascade on update cascade ;
-ALTER TABLE school_subject
+ALTER TABLE groupe_subject
 ADD CONSTRAINT subject_id
-FOREIGN KEY (subject_id) REFERENCES subject(id) on delete cascade on update cascade;
+FOREIGN KEY (subject_id) REFERENCES subject(id) on delete cascade on update cascade ;
+ALTER TABLE groupe_subject
+ADD CONSTRAINT group_id
+FOREIGN KEY (group_id) REFERENCES groupe(id) on delete cascade on update cascade;
 
 
 ALTER TABLE ttschool.groupe
